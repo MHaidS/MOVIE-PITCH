edd858d769c7cb9853625a5c01d5bc7e5ffadd7a
Reese-haids <reese.yes@gmail.com>
2023-12-07 23:44:42 +0800

    2nd commit - set up fetchSynopsis

--- index.js
+++ index.js
@@ -13,28 +13,42 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 document.getElementById("send-btn").addEventListener("click", () => {
-  setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`;
-  movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`;
+  if (setupTextarea.value) {
+    const userInput = setupTextarea.value;
+    setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`;
+    movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`;
 
-  fetchBotReply();
+    fetchBotReply(userInput);
+
+    fetchSynopsis(userInput);
+  }
+  // fetchBotReply();
 });
 
-function fetchBotReply() {
-  // ... set up a fetch rqst & pass in the url, then open up the object & the method is 'POST'...
-  fetch(url, {
-    method: "POST",
-    // ... then the headers, an obj. w/ 2 key-value pairs: 1st is the "Content-Type" w/c is the  "application/json"... & the 2nd is the Authorization & that would be a string w/ the word 'Bearer' & our API key...
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${apiKey}`,
-    },
-    // ... then we need the body & it's going to need a model & a prompt... so firstly, we say 'JSON.stringify' & then we pass in an object w/ model "text-davinci-003" & the prompt...
-    body: JSON.stringify({
-      model: "text-davinci-003",
-      prompt: "Sound enthusiastic in five words or less.",
-    }),
-  })
-    // ... let's chain some 'thens' so we can deal w/ the 'response'... so we call 'json' on the 'response'... & then we'll take the 'data' fr the 'response'...  & say 'movieBossText.innerText'... & now we need to access our completion & we've got it here: 'Eagerly excited!'... & to get it, we've got to say 'data.choices'... & 'choices' holds an array & we actually want what is in the 1st element of the array at position '0'... & then we want to access the 'text' property ...
-    .then((response) => response.json())
-    .then((data) => (movieBossText.innerText = data.choices[0].text));
+// ++++++++++++++++++++++++++++++++++++++
+
+async function fetchBotReply(outline) {
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+
+    prompt: `Generate a short message to enthusiastically say "${outline}" sounds interesting and that you need some minutes to think about it. Mention one aspect of the sentence.`,
+
+    max_tokens: 60, // defaults to 16
+  });
+
+  movieBossText.innerText = response.data.choices[0].text.trim();
+
+  console.log(response);
+}
+
+async function fetchSynopsis(outline) {
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+
+    prompt: `Generate an engaging, professional and marketable movie synopsis based on the following idea: ${outline}`,
+    max_tokens: 700,
+  });
+
+  document.getElementById("output-text").innerText =
+    response.data.choices[0].text.trim();
 }
